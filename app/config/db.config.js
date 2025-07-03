204096120d2f3ba0cd9ec4cfb96d0cf3df863382
Esteban Chavez Villalta <esteban.chavez@tecsup.edu.pe>
2025-07-03 02:28:07 -0500

    Fix: Configurar DB para producción con Railway

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 export default {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "MysqlPass2004",
-  DB: "testdb",
-  PORT: 3306,
+  HOST: process.env.DB_HOST || "localhost",
+  USER: process.env.DB_USER || "root", 
+  PASSWORD: process.env.DB_PASSWORD || "MysqlPass2004",
+  DB: process.env.DB_NAME || "testdb",
+  PORT: process.env.DB_PORT || 3306,
   dialect: "mysql",
   pool: {
     max: 5,
